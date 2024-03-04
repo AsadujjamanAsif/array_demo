79ae30eeaaf6273208583c8a7ac0e2f3f387627d
AsadujjamanAsif <147911379+AsadujjamanAsif@users.noreply.github.com>
2024-03-04 20:57:15 +0600

    first commit

---
+++ src/Array.java
@@ -0,0 +1,9 @@
+public class Array {
+    public static void main(String[] args) {
+        int[] num = { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100 };
+        System.out.println("Array Size: " + num.length);
+        for (int x : num) {
+            System.out.println(x);
+        }
+    }
+}
