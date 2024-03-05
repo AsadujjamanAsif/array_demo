d45d8d00fcc03636ea397c078afdf39ce2de2931
AsadujjamanAsif <147911379+AsadujjamanAsif@users.noreply.github.com>
2024-03-05 21:16:04 +0600

    farst commit

--- src/matrix.java
+++ src/matrix.java
@@ -5,6 +5,7 @@ public class matrix {
         Scanner input = new Scanner(System.in);
         int[][] A = new int[2][3];
         int[][] B = new int[2][3];
+        int[][] C = new int[2][3];
 
         System.out.println("Enter elementrts of A matrix: ");
         for (int row = 0; row < 2; row++) {
@@ -34,6 +35,14 @@ public class matrix {
         for (int row = 0; row < 2; row++) {
             for (int col = 0; col < 3; col++) {
                 System.out.print(" " + B[row][col]);
+            }
+            System.out.println();
+        }
+        System.out.println("Sum of A and B metrix: ");
+        for (int row = 0; row < 2; row++) {
+            for (int col = 0; col < 3; col++) {
+                C[row][col] = A[row][col] + B[row][col];
+                System.out.print("\t" + (C[row][col]));
             }
             System.out.println();
         }
